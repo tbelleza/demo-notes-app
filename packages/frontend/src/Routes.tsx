a65a35908643763727a2f0c144dfd4c5dab943c9
tbelleza <theresabelleza@gmail.com>
2023-09-02 10:40:40 -0700

    building react app

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -1,11 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./containers/Home.tsx";
 import NotFound from "./containers/NotFound.tsx";
+import Login from "./containers/Login.tsx";
+import Signup from "./containers/Signup.tsx";
+import NewNote from "./containers/NewNote.tsx";
+import Notes from "./containers/Notes.tsx";
 
 export default function Links() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/notes/new" element={<NewNote />} />
+      <Route path="/notes/:id" element={<Notes />} />
       <Route path="*" element={<NotFound />} />;
     </Routes>
   );
--- packages/frontend/src/config.ts
+++ packages/frontend/src/config.ts
@@ -1,4 +1,7 @@
 const config = {
+    // Frontend config
+    MAX_ATTACHMENT_SIZE: 5000000,
+    
     // Backend config
     s3: {
       REGION: import.meta.env.VITE_REGION,
