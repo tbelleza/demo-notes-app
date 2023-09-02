95766fbd60278af2bd3e4d1be16383c31854ce5b
tbelleza <theresabelleza@gmail.com>
2023-09-02 11:36:32 -0700

    fix home.tsx

--- packages/frontend/src/containers/Home.tsx
+++ packages/frontend/src/containers/Home.tsx
@@ -1,12 +1,12 @@
-import { useEffect, useState } from "react";
-import ListGroup from "react-bootstrap/ListGroup";
-import { useAppContext } from "../lib/contextLib";
-import "./Home.css";
+import { useState, useEffect } from "react";
 import { API } from "aws-amplify";
 import { NoteType } from "../types/note";
 import { onError } from "../lib/errorLib";
-import { LinkContainer } from "react-router-bootstrap";
 import { BsPencilSquare } from "react-icons/bs";
+import ListGroup from "react-bootstrap/ListGroup";
+import { LinkContainer } from "react-router-bootstrap";
+import { useAppContext } from "../lib/contextLib";
+import "./Home.css";
 
 export default function Home() {
   const [notes, setNotes] = useState<Array<NoteType>>([]);
@@ -25,6 +25,7 @@ export default function Home() {
       } catch (e) {
         onError(e);
       }
+
       setIsLoading(false);
     }
 
@@ -36,29 +37,29 @@ export default function Home() {
   }
 
   function formatDate(str: undefined | string) {
-    return !str ? "" : new Date(str).toLocaleDateString();
+    return !str ? "" : new Date(str).toLocaleString();
   }
 
-  function renderNotesList(notes: { [key: string | symbol]: any }) {
+  function renderNotesList(notes: NoteType[]) {
     return (
       <>
         <LinkContainer to="/notes/new">
-          <ListGroup.Item action className="py-3 text-nonwrap text-truncate">
+          <ListGroup.Item action className="py-3 text-nowrap text-truncate">
             <BsPencilSquare size={17} />
-            <span className="ms-2  fw-bold">Create a new note</span>
+            <span className="ms-2 fw-bold">Create a new note</span>
           </ListGroup.Item>
         </LinkContainer>
-        {notes.map(({ noteId, content, createdAt }) => {
+        {notes.map(({ noteId, content, createdAt }) => (
           <LinkContainer key={noteId} to={`/notes/${noteId}`}>
-            <ListGroup.Item action className="text-nonwrap text-truncate">
+            <ListGroup.Item action className="text-nowrap text-truncate">
               <span className="fw-bold">{content.trim().split("\n")[0]}</span>
               <br />
-              <span className="text-mutes">
+              <span className="text-muted">
                 Created: {formatDate(createdAt)}
               </span>
             </ListGroup.Item>
           </LinkContainer>
-        })}
+        ))}
       </>
     );
   }
@@ -69,16 +70,16 @@ export default function Home() {
         <h1>Scratch</h1>
         <p className="text-muted">A simple note taking app</p>
       </div>
-    )
+    );
   }
 
   function renderNotes() {
-    return(
-      <div>
+    return (
+      <div className="notes">
         <h2 className="pb-3 mt-4 mb-3 border-bottom">Your Notes</h2>
         <ListGroup>{!isLoading && renderNotesList(notes)}</ListGroup>
       </div>
-    )
+    );
   }
 
   return (
